bb5b4cb2b97b99ab96f44a4f4c390f08efd19353
wantbot_adm <wantbot_adm>
2023-02-06 09:54:49 -0300

    Relembrando Dicionarios

--- w01.py
+++ w01.py
@@ -1,3 +1,14 @@
 # Primeira sprint
 
-print('Ola mundo!')
+print('Ola Atenção!')
+
+lista = ['Ricardo', 'Sidney', 'Kele', 'Lucas']
+
+familia = {'Pai':'Sidney','Mãe':'Kele','TemFilhos':False}
+
+familia2 = dict(Pai='Carlos',Mae='Cristina',TemFilhos=True)
+
+lista_tuplas = [('pai','Joao',('Mae','maria'),('TemFilhos',False))]
+
+print(familia2['Pai'])
+familia2.values()
