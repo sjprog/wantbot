2bd0324947fe5f1a3399c40c46d46af4a1656b96
wantbot_adm <wantbot_adm>
2023-02-06 10:14:06 -0300

    Revisando...

--- w01.py
+++ w01.py
@@ -12,3 +12,19 @@ lista_tuplas = [('pai','Joao',('Mae','maria'),('TemFilhos',False))]
 
 print(familia2['Pai'])
 familia2.values()
+
+
+class Veiculo:
+    def __init__(self,nome,tipo):
+        self._nome = nome
+        self. tipo = tipo
+    
+    def tipoVeiculo(self):
+        print(self._nome)
+
+v2 = Veiculo('Caminhao','Terrestre')
+
+v2.tipoVeiculo()
+
+
+
