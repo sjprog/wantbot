90daade16322db63e76d7f036850bf91c726ddec
wantbot_adm <wantbot_adm>
2023-02-08 09:49:59 -0300

    Revisando request e json

--- RequestEstudos.py
+++ RequestEstudos.py
@@ -1,12 +1,29 @@
 import requests
 
-resp = requests.get("https://viacep.com.br/ws/01001000/json/")
+# resp = requests.get("https://viacep.com.br/ws/01001000/json/")
 
-# print(resp.text)
+# # print(resp.text)
 
-dd = resp.json()
+# dd = resp.json()
 
-dt = resp.text
+# dt = resp.text
 
-print(dd)
+# print(dd)
 
+
+def get_info_cep(cep):
+
+    url_base = f'https://viacep.com.br/ws/{cep}/json/'
+    r = requests.get(url_base)
+    return r.json()
+
+d = get_info_cep('71825115')
+
+# print(d)
+
+
+def retorna_rua(d):
+    return d['logradouro']
+
+
+print(retorna_rua(d))
