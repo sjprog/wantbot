789275567b91d370c0e40e9cb98f78b273c81b8f
wantbot_adm <wantbot_adm>
2023-02-14 09:45:58 -0300

    Api de filmes

--- RequestEstudos.py
+++ RequestEstudos.py
@@ -33,12 +33,22 @@ import requests
 
 # ===========================================================================
 
-def get_ticks(moeda='BTC',metodo='ticker'):
+# def get_ticks(moeda='BTC',metodo='ticker'):
 
-    url_base = f'https://www.mercadobitcoin.net/api/{moeda}/{metodo}/'
+#     url_base = f'https://www.mercadobitcoin.net/api/{moeda}/{metodo}/'
+#     r = requests.get(url_base)
+#     return r.json()
+
+# d = get_ticks('ETH')
+
+# print(d)
+
+# =============================================================================
+
+def get_movie(nome='matrix'):
+
+    url_base = f'http://www.omdbapi.com/?apikey=83b61bea&t={nome}'
     r = requests.get(url_base)
     return r.json()
 
-d = get_ticks('ETH')
-
-print(d)
+print(get_movie())
