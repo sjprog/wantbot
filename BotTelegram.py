60cd1b12c70c0c5f4356d149db0bb054f6754b38
wantbot_adm <wantbot_adm>
2023-03-24 10:35:13 -0300

    Respondendo

--- BotTelegram.py
+++ BotTelegram.py
@@ -14,17 +14,30 @@ class TelegramBot:
     def Iniciar(self):
         update_id = None
         while True:
-            updates = self.obter_updates()
+            updates = self.obter_updates()            
 
             dados = updates["result"]
 
             print(dados)
-            update_id = dados[0]['update_id']
-            print("Update ID = ", update_id)
-            
+            update_id = dados[-1]['update_id']
 
+            mensagem = str(dados[0]["message"]["text"])
+            chat_id_ = dados[-1]["message"]["from"]["id"]
+
+            nome = dados[-1]["message"]["from"]["first_name"]
+
+            print("Chat ID = ", chat_id_)
+            print('Mensagem = ', mensagem)
+
+            self.responder("Olá "+nome+" Meu nome é Robô Telegram. ",chat_id_)
             print("------------------------------")
+
             time.sleep(5)
+
+    def responder(self, resposta, chat_id):
+        link_requisicao = f'{self.url_base}sendMessage?chat_id={chat_id}&text={resposta}'
+        requests.get(link_requisicao)
+
 
     def obter_updates(self):
 
@@ -34,3 +47,5 @@ class TelegramBot:
         return r.json()
        
 
+t = TelegramBot()
+t.Iniciar()
