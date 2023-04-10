5ed699b72b868c1eab15b74250ed00e3e8fde2ce
wantbot_adm <wantbot_adm>
2023-04-10 11:14:49 -0300

    Biblioteca telepot

--- Bilioteca_TelePot.py
+++ Bilioteca_TelePot.py
@@ -1,6 +1,7 @@
 # instalando o telepot = pip install telepot
 
 
+import time
 from pprint import pprint
 
 import telepot as tp
@@ -12,13 +13,32 @@ bot = tp.Bot(token)
 
 # print(bot.getMe())
 
-response = bot.getUpdates()
-pprint(response)
+# response = bot.getUpdates()
+# pprint(response)
 
-resp = bot.sendMessage(1043367435, 'Ola, eu sou o WantBot!')
-resp = bot.sendMessage(1043367435, 'Como posso Ajudar?')
+# resp = bot.sendMessage(1043367435, 'Ola, eu sou o WantBot!')
+# resp = bot.sendMessage(1043367435, 'Como posso Ajudar?')
+
+# def handle(msg):
+#     pprint(msg)
+
+# MessageLoop(bot, handle).run_as_thread()
+
 
 def handle(msg):
-    pprint(msg)
+    content_type, chat_type, chat_id = tp.glance(msg)
+    print(content_type, chat_type, chat_id)
 
+    if content_type == 'text':
+        bot.sendMessage(chat_id, msg['text']+ " <= Texto repetido.")
+
+    if content_type == 'audio':
+        pass
+
+
+# bot = tp.Bot(token)
 MessageLoop(bot, handle).run_as_thread()
+print('Listening ...')
+
+while 1:
+    time.sleep(10)
