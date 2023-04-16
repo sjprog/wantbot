bba35db29e81be8ccd806ce5fc774437e11a29bc
wantbot_adm <wantbot_adm>
2023-04-16 14:55:08 -0300

    Corrigindo o erro do filters

--- python_telegram_Bot_1.py
+++ python_telegram_Bot_1.py
@@ -1,5 +1,5 @@
 import telegram
-from telegram.ext import CommandHandler, Updater
+from telegram.ext import CommandHandler, MessageHandler, Updater, filters
 
 from token_1 import token_s
 
@@ -11,8 +11,21 @@ dispatcher = updater.dispatcher
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Ola, eu sou o robo telegram!")
 
-start_handler = CommandHandler('start', start)
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="repete = " +update.message.text)
 
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(filters.TEXT & (~ filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
 
 updater.start_polling()
